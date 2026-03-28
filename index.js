c45471882b33b6ff006963de227b7d04c071ae01
Gabriel Ferreira Ferraz <gabriel.ferrazferreira03@gmail.com>
2026-03-28 09:54:02 -0300

    mais conteúdo

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let string = "Julio";
-let number = 1234;
-let boolean = true;
-let undefined;
-let indefinido = null;
+let string = "Julio"; //Textos.
+let number = 1234; //Inteiros e decimais.
+let boolean = true; //Verdadeiro ou falso.
+let undefined; //Variavel declarada, mas sem valor.
+let indefinido = null; //Ausencia de valor.
 
 console.log("Hello Word");
+
+if (num1 == 5 && num2 == 5) {
+    //verdadeiro
+}
+
+if (num1 == 5 || num2 == 5) {
+    //verdadeiro
+}
+
+const ativo = false;
+if (!ativo) {
+    console.log("O usuário não stá ativo.")
+}
