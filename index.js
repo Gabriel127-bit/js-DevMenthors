917dcfa0dd9b9fd4bb7590de67c85252bdd73f57
Gabriel Ferreira Ferraz <gabriel.ferrazferreira03@gmail.com>
2026-03-28 11:49:56 -0300

    final

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let string = "Julio"; //Textos.
-let number = 1234; //Inteiros e decimais.
-let boolean = true; //Verdadeiro ou falso.
-let undefined; //Variavel declarada, mas sem valor.
-let indefinido = null; //Ausencia de valor.
+// let string = "Julio"; //Textos.
+// let number = 1234; //Inteiros e decimais.
+// let boolean = true; //Verdadeiro ou falso.
+// let undefined; //Variavel declarada, mas sem valor.
+// let indefinido = null; //Ausencia de valor.
+
+let num1 = 5;
+let num2 = 5;
 
 console.log("Hello Word");
 
@@ -17,4 +20,18 @@ if (num1 == 5 || num2 == 5) {
 const ativo = false;
 if (!ativo) {
     console.log("O usuário não stá ativo.")
+}
+
+//---------------------------------------------------------
+let corFavorita 
+
+switch (corFavorita) {
+    case "Vermelho":
+        console.log("A cor é quente.");
+        break;
+    case "Azul":
+        console.log("A cor é fria.")
+        break;
+    default:
+        console.log("Cor não identificada.")
 }
